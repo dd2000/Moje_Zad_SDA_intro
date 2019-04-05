5a3cc67009a6dfa1d1b4f556f36b751d920443ac
Dasz.Berg <dd2000@wp.pl>
2019-04-05 21:30:36 +0200

    05 kwietnia 2019

--- src/pl/sda/Zad_01I_arrays_slajd_123/Arrays4Test.java
+++ src/pl/sda/Zad_01I_arrays_slajd_123/Arrays4Test.java
@@ -3,7 +3,7 @@ package pl.sda.Zad_01I_arrays_slajd_123;
 4. Napisz metodę, która jako parametr przyjmuje tablicę typu float[]
    i w wyniku swojego działania powoduje podwojenie wartości dla każdego indeksu przekazanej tablicy.
 */
-public class Array4Test {
+public class Arrays4Test {
     public static void main(String[] args) {
         float[] ftab = {3.4f,-5,5.2f,-1,0,0.5f};
         System.out.println("\nWartości tablicy źródłowej:");
@@ -30,4 +30,4 @@ public class Array4Test {
             tab[i] = 2*tab[i];
         return tab;
     } // podwojenie();
-} // class Array4Test
+} // class Arrays4Test
